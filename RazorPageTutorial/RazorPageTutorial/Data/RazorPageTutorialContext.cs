cb901a4135fee81133e06880428bca067d3181c3
Alan <dev@alanhall.co.uk>
2019-05-17 13:19:24 +0100

    Add a model - Scaffold the movie model

---
+++ RazorPageTutorial/RazorPageTutorial/Data/RazorPageTutorialContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPageTutorial.Models
+{
+    public class RazorPageTutorialContext : DbContext
+    {
+        public RazorPageTutorialContext (DbContextOptions<RazorPageTutorialContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPageTutorial.Models.Movie> Movie { get; set; }
+    }
+}
